90e60aaeacbe015f9005313e2a16b18d10429113
rajagd <71009166+rajagd@users.noreply.github.com>
2020-09-11 10:33:26 +0200

    Create Main.js

---
+++ js/Main.js
@@ -0,0 +1,11 @@
+//	DOMContentLoaded afin de charger le js sans attendre le CSS, images etc....
+
+
+document.addEventListener( "DOMContentLoaded", function() {
+
+    let slider = new Slider("#showSliderBloc");
+    let map = new Map("#showMapBloc");
+    let reservation = new Reservation(".showReservations");
+    let canvas = new Canvas("#canvasBloc");
+
+});

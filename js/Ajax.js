2153389249ba474c2b1794ff8e930866e56c39d4
rajagd <71009166+rajagd@users.noreply.github.com>
2020-09-11 10:32:44 +0200

    Create Ajax.js

---
+++ js/Ajax.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function ajaxGet(url, callback) {
+    let req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur" + url);
+    });
+    req.send(null);
+}
